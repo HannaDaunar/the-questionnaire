276689cd08933d05dbaaa28900bcc8af3418e611
HannaDaunar <hannadaunar@yandex.by>
2024-11-22 13:53:20 +0100

    домашнее задание 10

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
 const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+    // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
+    // https://learn.javascript.ru/default-browser-action
+    event.preventDefault();
+    // Здесь твой код
+    const name = document.querySelector("#name").value;
+    const secondName = document.querySelector("#secondName").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const checkbox = document.querySelector("#agree");
 
-  // Здесь твой код
+    function controlCheckbox() {
+        if (checkbox.checked) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const button = document.querySelector("#button");
+    const clear = document.querySelector("#clear");
+    const notification = document.querySelector(".notification-wrapper");
+    const notificationText = document.querySelector(".notification-text");
+
+    function showNotification() {
+        notification.classList.remove("hidden");
+        setTimeout(() => {
+            notification.classList.add("hidden");
+        }, 5000)
+    }
+
+
+    button.addEventListener('click', function() {
+        fetch(`https://polinashneider.space/user`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer: HannaDaunar'
+                },
+                body: JSON.stringify({
+                    "name": name,
+                    "secondName": secondName,
+                    "phone": phone,
+                    "email": email,
+                    "agree": controlCheckbox()
+                })
+            })
+            .then((result) => {
+                return result.json();
+            })
+            .then((data) => {
+                console.log(data);
+                notificationText.textContent = ":) Вы успешно зарегистрированы!"
+                form.reset();
+                showNotification();
+
+            })
+            .catch((error) => {
+                notificationText.textContent = ":( Произошла ошибка, попробуйте еще раз"
+                showNotification();
+            })
+    })
+
+    clear.addEventListener('click', function() {
+        form.reset();
+    })
 });
